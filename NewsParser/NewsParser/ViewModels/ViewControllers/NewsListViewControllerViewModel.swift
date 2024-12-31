e2011bafe058dec16bc894a94a1fefd57e454dfb
test <rsprikut@mail.ru>
2024-12-31 18:39:05 +0200

    Implement RSS fetching method

--- NewsParser/NewsParser/ViewModels/ViewControllers/NewsListViewControllerViewModel.swift
+++ NewsParser/NewsParser/ViewModels/ViewControllers/NewsListViewControllerViewModel.swift
@@ -15,14 +15,15 @@ protocol NewsListViewControllerViewModel {
     var numberOfSections: Int { get }
     func numberOfItemsInSection(section: Int) -> Int
     
-    func itemAtIndex(indexPath: IndexPath) -> RSSItem
+    func itemAtIndex(indexPath: IndexPath) -> RSSItemRaw
     
-    func startFetchingIfNeeded()
+    func startFetchingIfNeeded() async
 }
 
 final class NewsListViewControllerViewModelImpl: NewsListViewControllerViewModel {
     private let queue: DispatchQueue = DispatchQueue(label: "com.newsListViewModel.queue", attributes: .concurrent)
-    private var newsSources: [RSSItem] = [] {
+    
+    private var newsSources: [RSSItemRaw] = [] {
         didSet {
             delegate?.reloadData()
         }
@@ -38,15 +39,46 @@ final class NewsListViewControllerViewModelImpl: NewsListViewControllerViewModel
         return newsSources.count
     }
     
-    func itemAtIndex(indexPath: IndexPath) -> RSSItem {
+    func itemAtIndex(indexPath: IndexPath) -> RSSItemRaw {
         return newsSources[indexPath.row]
     }
     
-    func startFetchingIfNeeded() {
-        queue.async {
-            Task { [weak self] in
-                self?.newsSources = await RSSParserManager().fetchMultipleRSSFeeds(urls: ["https://www.vedomosti.ru/rss/articles.xml"])
-                    .sorted(by: { $0.pubDate > $1.pubDate })
+    func startFetchingIfNeeded() async {
+        await withCheckedContinuation { continuation in
+            queue.async {
+                let newsSources = DatabaseManager.shared.fetch(NewsSource.self)
+
+                for newsSource in newsSources {
+                    let existingLinks = Set(newsSource.news.map { $0.link })
+                    let stringURL = newsSource.stringURL
+
+                    do {
+                        let items = try RSSParserManager().fetchAndParseRSS(from: stringURL)
+                            .filter { !existingLinks.contains($0.link) }
+
+                        if items.count > 0 {
+                            DatabaseManager.shared.update {
+                                if newsSource.name == nil {
+                                    newsSource.name = items.first?.sourceTitle
+                                }
+
+                                newsSource.news.append(objectsIn: items.map {
+                                    let realmRSS = RSSItem()
+                                    realmRSS.newsDescription = $0.description
+                                    realmRSS.sourceTitle = $0.sourceTitle
+                                    realmRSS.title = $0.title
+                                    realmRSS.imageLink = $0.imageLink
+                                    realmRSS.pubDate = $0.pubDate
+                                    realmRSS.link = $0.link
+                                    return realmRSS
+                                })
+                            }
+                        }
+                    } catch {
+                        print("Error during parsing RSS items: \(error)")
+                    }
+                }
+                continuation.resume()
             }
         }
     }
