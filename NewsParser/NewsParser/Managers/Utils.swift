5feb84152bef984e6d7e8303f034152a2f5c599a
Rodion <rosprikut@gmail.com>
2025-01-12 23:28:35 +0200

    Add functions for creating pop-ups

--- NewsParser/NewsParser/Managers/Utils.swift
+++ NewsParser/NewsParser/Managers/Utils.swift
@@ -27,15 +27,43 @@ struct Utils {
         return formatter.string(from: date)
     }
     
-    static func makePopUp(parent: UIViewController, title: String?, message: String?, actionTitle: String, actionStyle: UIAlertAction.Style, cancelTitle: String?, actionHandler: ((UIAlertAction) -> Void)?, cancelHandler: ((UIAlertAction) -> Void)?) {
+    static func makePopUp(parent: UIViewController, title: String?, message: String?, actionTitle: String, actionStyle: UIAlertAction.Style, cancelTitle: String? = nil, actionHandler: (() -> Void)? = nil, cancelHandler: (() -> Void)? = nil) {
         let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let action = UIAlertAction(title: actionTitle, style: actionStyle, handler: actionHandler)
+        let action = UIAlertAction(title: actionTitle, style: actionStyle, handler: { _ in
+            actionHandler?()
+        })
         controller.addAction(action)
         
         var cancelAction: UIAlertAction?
         if cancelTitle != nil {
-            cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelHandler)
+            cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: { _ in
+                cancelHandler?()
+            })
+            controller.addAction(cancelAction!)
+        }
+        
+        parent.present(controller, animated: true)
+    }
+
+    static func addPopupWithTextfield(parent: UIViewController, title: String?, message: String?, textfieldDelegate: UITextFieldDelegate?, actionTitle: String, actionStyle: UIAlertAction.Style, cancelTitle: String?, actionHandler: (() -> Void)?, cancelHandler: (() -> Void)?) {
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        controller.addTextField() { textfield in
+            textfield.delegate = textfieldDelegate
+            textfield.placeholder = "Введите URL"
+        }
+        
+        let action = UIAlertAction(title: actionTitle, style: actionStyle, handler: { _ in
+            actionHandler?()
+        })
+        controller.addAction(action)
+        
+        var cancelAction: UIAlertAction?
+        if cancelTitle != nil {
+            cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: { _ in
+                cancelHandler?()
+            })
             controller.addAction(cancelAction!)
         }
         
