8a666c41af13c92999e0433d1471d8a84f974901
Rodion <rosprikut@gmail.com>
2025-01-12 23:29:45 +0200

    Add String extensions, remove Main.storyboard leftovers

---
+++ NewsParser/NewsParser/Extensions/String+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  String+Extensions.swift
+//  NewsParser
+//
+//  Created by Rodion on 12.01.2025.
+//
+
+import Foundation
+
+extension String {
+    func isValidURL() -> Bool {
+        let pattern = #"^https?://(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}/.*"#
+        
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) else {
+            return false
+        }
+        
+        let range = NSRange(location: 0, length: self.utf16.count)
+        return regex.firstMatch(in: self, options: [], range: range) != nil
+    }
+    
+    func removeUrlPrefix() -> String {
+        let pattern = "^(https?://)?(www\\.)?"
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else {
+            return self
+        }
+        
+        let range = NSRange(location: 0, length: self.utf16.count)
+        let modifiedString = regex.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "")
+        
+        return modifiedString
+    }
+}
